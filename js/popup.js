2fe3707c25ead27d6d247c182c81592cd6e8a509
Marie Marchal <you@example.com>
2025-01-21 17:00:28 +0100

    valeur prénom

--- js/popup.js
+++ js/popup.js
@@ -1,2 +1,8 @@
-API KEY : 619e23a1838ec55b4f222b07b358b2a9
-Name KEY : 	Default
+const token = "e4e24883b03b7cf84d3f4079a52b044d99d5f698b18a089c1cd00bdb567bb4b8"
+let valideBtn = document.querySelector("#validBtn")
+let input = document.querySelector ("#champs")
+
+valideBtn.addEventListener ('click', () => {
+    let userName = input.value
+    console.log(userName)
+})
