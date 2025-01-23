a475e88ab6f28ec4297a40fb721ac6e0b1bb5ed0
Marie Marchal <you@example.com>
2025-01-23 12:06:51 +0100

    prise d'infos utilisateur bug

--- js/popup.js
+++ js/popup.js
@@ -1,8 +1,36 @@
 const token = "e4e24883b03b7cf84d3f4079a52b044d99d5f698b18a089c1cd00bdb567bb4b8"
 let valideBtn = document.querySelector("#validBtn")
 let input = document.querySelector ("#champs")
+let textContent = document.querySelector("#contentText")
+let userName =""
+let userCity =""
 
-valideBtn.addEventListener ('click', () => {
-    let userName = input.value
-    console.log(userName)
-})
+function whatUser() {
+    valideBtn.addEventListener ('click', () => {
+        userName = input.value
+        console.log(userName)
+        chrome.storage.local.set({user : userName})
+        textContent.innerText = `Bonjour ${userName}, quel est le nom de ta ville ?`
+        whatCity()
+      })
+}
+
+function whatCity() {
+    input.value = ""
+    valideBtn.addEventListener ('click', () => {
+        userCity = input.value
+        console.log(userCity)
+        chrome.storage.local.set({city : userCity})
+        textContent.innerText =""
+        valideBtn.style.display = "none"
+        input.style.display ="none"
+        loadPage()
+
+    })
+}
+
+function loadPage() {
+    textContent.innerText = `Bonjour ${userName}, tu habites à ${userCity}`
+}
+
+whatUser()
